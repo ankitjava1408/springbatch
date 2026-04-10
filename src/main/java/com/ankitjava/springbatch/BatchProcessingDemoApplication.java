588aa6ed2fd8defd1344d91e4e6762761adc8555
Ankit Chourasia <ankitchaurasia1408@gmail.com>
2026-04-10 21:30:17 +0530

    Initial Spring Boot Batch Processing project setup

---
+++ src/main/java/com/ankitjava/springbatch/BatchProcessingDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ankitjava.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchProcessingDemoApplication.class, args);
+        System.out.println("Batch Processing System");
+    }
+
+}
